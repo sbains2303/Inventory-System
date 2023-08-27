b6c6f76c1a9c2a67dfe8c093ea084419f1250986
sbains2303 <ia22870@bristol.ac.uk>
2023-08-27 15:46:57 +0100

    Sale predictions

--- my_module/ai.py
+++ my_module/ai.py
@@ -9,60 +9,70 @@ class SmartInventoryManagementSystem:
         self.stock_data = stock_data
 
     def integrate_data(self):
-        sales_df = self.sales_data
-        stock_df = self.stock_data
-
-        return sales_df, stock_df
+        # Combine sales and stock data, if needed
+        return self.sales_data, self.stock_data
 
     def demand_forecasting(self, sales_df):
-        # Perform time series analysis and machine learning algorithms for demand forecasting
+        # Time series analysis and machine learning for demand forecasting
+
+        # Preprocessing sales data
         df = sales_df
-        # Converts 'date' column to datetime format and subtracts 7 from each date to shift back by week
-        df['date'] = pd.to_datetime(df['date']) - pd.to_timedelta(7, unit='d')
-        # Filters df to include 'date' and 'sales' and groups data by weekly Mondays (intervals)
-        df = df.filter(['date', 'sales']).groupby([pd.Grouper(key='date', freq='W-MON')])
+        df['Date'] = pd.to_datetime(df['Date']) - pd.to_timedelta(7, unit='d')
+        df = df[['Date', 'Sale']]  # Select relevant columns
 
-        # Adds new 'shift_sale' column to df, containing 'sales' values shifted by one position (previous week's sales)
-        df['shift_sale'] = df['sales'].shift(1)
-        # Drops first row of df due to shift operation
+        # Create a shifted column for previous week's sales
+        df['shift_sale'] = df['Sale'].shift(1)
+
+        # Group data by weekly intervals (Mondays) and aggregate sales
+        df = df.groupby([pd.Grouper(key='Date', freq='W-MON')]).agg({'Sale': 'sum', 'shift_sale': 'sum'})
+
+        # Remove the first row (due to the shift operation) and calculate a four-week average
         df = df.iloc[1:]
-        # Calculates four-week average for 'sales' column and assigns to new column
-        df['week_avg'] = self.four_week_avg(df['sales'].tolist())
+        df['week_avg'] = self.four_week_avg(df['Sale'].tolist())
 
-        # Creates new df 'test' by selecting last 52 rows for testing
-        self.test = df.iloc[-52:]
-        # Modifies original df by excluding last 52 rows to create a training set
-        df = df.iloc[:-52]
+        # Create a test dataset with the last 26 weeks and a training dataset
+        self.test = df.iloc[-26:]
+        df = df.iloc[:-26]
 
-        # df is split into feature data 'X' (all columns except 'sales') and target data 'y' ('sales' column)
-        X = df.drop('sales', axis=1)
-        y = df['sales']
-        # Training data 'X' and 'y' are split into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-        # SVR model is created to fit the model using the training data, and training it to predict the target 'sales' based on features
-        clf = svm.SVR(C = 1, kernel = 'linear', degree = 8, gamma = 'scale', coef0= 10)
+        # Split data into features 'X' (excluding 'Sale') and target 'y' ('Sale')
+        X = df.drop('Sale', axis=1)
+        y = df['Sale']
+
+        # Split the training data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        # Create and train the Support Vector Regression (SVR) model
+        clf = svm.SVR(C=1, kernel='linear', degree=8, gamma='scale', coef0=10)
         clf.fit(X_train, y_train)
-        # Uses trained SVR model to predict 'sales' values for test set 'X_test'
-        predictions = clf.predict(X_test)
-        # Uses trained model to predict 'sales' values for 'test' df (last 52 weeks)
-        self.predictions = clf.predict(self.test.drop('week_sale', axis = 1))
 
-    def four_week_avg(self,sales):
+        # Make predictions for the test dataset
+        self.test = self.test.drop('Sale', axis=1)  # Prepare the test data
+        self.predictions = clf.predict(self.test)
+
+    def four_week_avg(self, sales):
+        # Calculate a four-week average
+
         sum = 0
         week_avg = []
+
+        # Calculate the average for the last 4 weeks
         for i in range(3, -1, -1):
             for j in range(i):
                 sum += sales[j]
             if (i != 0):
-                week_avg.append(sum/i)
+                week_avg.append(sum / i)
             sum = 0
+
         week_avg.append(sales[0])
         week_avg.reverse()
+
+        # Calculate the average for the remaining weeks
         for row in range(len(sales) - 4):
             for row in range(row, row + 4):
                 sum += sales[row]
-            week_avg.append(sum/4)
+            week_avg.append(sum / 4)
             sum = 0
+
         return week_avg
 
     def optimize_inventory(self):
@@ -70,8 +80,10 @@ class SmartInventoryManagementSystem:
         self.demand_forecasting(sales_df)
 
     def getTest(self):
-        return self.test.index
+        # Get the test dataset (last 26 weeks)
+        return self.test
 
     def getPredictions(self):
+        # Get the predictions made by the model
         return self.predictions
 
--- my_module/insights.py
+++ my_module/insights.py
@@ -1,58 +1,79 @@
 import customtkinter
+import pandas as pd
 from ai import SmartInventoryManagementSystem
 from my_module.connection import sql_query, py_conn
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 
+
 class InsightsWindow:
 
     def __init__(self, master):
         self.master = master
-        self.master.geometry("600x400")
+        self.master.geometry("800x600")
         self.master.title("Insights")
 
-        self.label = customtkinter.CTkLabel(self.master, text="Insights", font=('Helvetica',20,'bold'))
-        self.label.place(x=260, y=5)
-        self.chlabel = customtkinter.CTkLabel(self.master, text="Type product name below:", font=('Helvetica',14,'bold'))
-        self.chlabel.place(x=30, y=50)
+        # Create and place labels and entry widget
+        self.label = customtkinter.CTkLabel(self.master, text="Insights", font=('Helvetica', 20, 'bold'))
+        self.label.place(x=350, y=5)
 
+        self.create_label("Type product name below:", 30, 50,'white')
         self.product = customtkinter.CTkEntry(self.master)
         self.product.place(x=30, y=80)
+        self.create_button("Show", self.getProduct, 30, 120)
 
-        self.button = customtkinter.CTkButton(self.master, command=lambda: self.getProduct(), text="Show")
-        self.button.place(x=30, y=120)
+    # Creating label in customertkinter
+    def create_label(self, text, x, y,colour):
+        customtkinter.CTkLabel(self.master, text=text, font=('Helvetica', 14, 'bold'),text_color=colour).place(x=x, y=y)
 
+    # Creating button in customtkinter
+    def create_button(self, text, command, x, y):
+        customtkinter.CTkButton(self.master, command=command, text=text).place(x=x, y=y)
+
+    # Get product id and call ai for sales calculations
     def getProduct(self):
-        variable = []
-        variable.append(self.product.get())
-        self.id = sql_query('SELECT ProductID FROM Products WHERE Name=%s', variable)
-        rid = [row[0] for row in self.id]
-        if rid:
-            product_id=rid[0]
-            self.callai(product_id)
+        product_name = self.product.get()
+        product_id = self.get_product_id(product_name)
+        if product_id:
+            self.call_ai(product_id)
 
-    def callai(self, productID):
-        sales_data = py_conn(0, productID)  # Replace with actual sales data file name
-        print(sales_data)
-        stock_data = py_conn(1, productID)  # Replace with actual stock data file name
+    # Retrieves product id from MySQL database
+    def get_product_id(self, product_name):
+        result = sql_query('SELECT ProductID FROM Products WHERE Name=%s', [product_name])
+        return result[0][0] if result else None
+
+    # Creates new inventory_management_system instance and gets predictions values
+    def call_ai(self, product_id):
+        sales_data = py_conn(0, product_id)  # Replace with actual sales data file name
+        stock_data = py_conn(1, product_id)  # Replace with actual stock data file name
+
         inventory_management_system = SmartInventoryManagementSystem(sales_data, stock_data)
         inventory_management_system.optimize_inventory()
         self.test = inventory_management_system.getTest()
         self.predictions = inventory_management_system.getPredictions()
+        self.test['predicted_week_avg'] = self.predictions
 
         # Create a Matplotlib figure
-        fig, ax = plt.subplots(figsize=(10, 6))
+        fig, ax = plt.subplots(figsize=(10, 5))
 
-        plt.figure(figsize=(10, 6))
-        # 'test.index' represents the time axis
-        plt.plot(self.test.index, self.test['sales'], label='Actual Sales')
-        plt.plot(self.test.index, self.predictions, label='Predicted Sales', linestyle='dashed')
-        plt.xlabel('Time')
+        # Ensure the date format in 'self.test' index matches the date format used in 'start_date' and 'end_date'
+        self.test.index = pd.to_datetime(self.test.index)
+
+        plt.xlim(self.test.index.min(), self.test.index.max())
+        plt.ylim(0, 80)
+
+        plt.xlabel('Week in Year')
         plt.ylabel('Sales')
         plt.title('Actual vs Predicted Sales')
-        plt.legend()
         plt.tick_params(axis='x', rotation=45)
+        plt.plot(self.test.index, self.test['week_avg'], label='Actual Sales')
+        plt.plot(self.test.index, self.test['predicted_week_avg'], label='Predicted Sales', linestyle='dashed')
 
         canvas = FigureCanvasTkAgg(fig, master=self.master)
         canvas_widget = canvas.get_tk_widget()
-        canvas_widget.place(x=50, y=20)
+        canvas_widget.config(width=800, height=800)
+        canvas_widget.place(x=350, y=80)
+
+        self.create_label("Solid line = Actual sales", 30, 220,'light blue')
+        self.create_label("Dashed line = Predicted sales", 30, 240, 'orange')
+        self.create_label("derived from previous sales", 30, 260, 'orange')
